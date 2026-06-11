31a03e5589fb1a37e1935ecfee623144ae299684
Saurabh Kumar <saurabh.k0402@gmail.com>
2026-06-11 20:58:03 +0530

    feat: setup tool call with streaming

--- 5-mcp/consumer.py
+++ 5-mcp/consumer.py
@@ -10,6 +10,9 @@ client = OpenAI(
 )
 
 async def main():
+    print('Ask me something... > ', end='', flush=True)
+    user_prompt = input()
+
     async with streamable_http_client('http://localhost:8000/mcp') as (read, write, _):
         async with ClientSession(read, write) as session:
             await session.initialize()
@@ -43,36 +46,90 @@ async def main():
                     'role': 'user',
                     'content': [{
                         'type': 'text',
-                        'text': 'what is the weather in Bengaluru?'
+                        'text': user_prompt,
                     }]
                 }
             ]
             
             while True:
-                response = client.chat.completions.create(
+                stream = client.chat.completions.create(
                     model='models/gemma-4-E4B-it-Q4_K_M.gguf',
                     messages=messages,
                     tools=mcp_tools,
+                    stream=True
                 )
 
-                message = response.choices[0].message
-                messages.append(message)
+                tool_calls = {}
+                thinking = False
+                outputting = False
 
-                if not message.tool_calls:
-                    print(message.content)
+                for chunk in stream:
+                    delta = chunk.choices[0].delta
+
+                    if getattr(delta, 'reasoning_content', None):
+                        if not thinking:
+                            print('\n\033[34m🤔 Thinking...\033[0m')
+                            thinking = True
+                        
+                        print(f'\033[2m{delta.reasoning_content}\033[0m', end="", flush=True)
+                    
+                    if delta.content:
+                        if not outputting:
+                            print('\n------------------------------------------------------------------')
+                            outputting = True
+
+                        print(delta.content, end='', flush=True)
+
+                    if delta.tool_calls:
+                        for tc in delta.tool_calls:
+                            index = tc.index
+
+                            if index not in tool_calls:
+                                tool_calls[index] = {
+                                    'id': '',
+                                    'name': '',
+                                    'arguments': ''
+                                }
+
+                            if tc.id:
+                                tool_calls[index]['id'] += tc.id
+
+                            if tc.function and tc.function.name:
+                                tool_calls[index]['name'] += tc.function.name
+
+                            if tc.function.arguments:
+                                tool_calls[index]['arguments'] += tc.function.arguments
+
+                if not tool_calls:
                     break
 
-                for tool in message.tool_calls:
-                    name = tool.function.name
-                    args = json.loads(tool.function.arguments)
+                messages.append({
+                    'role': 'assistant',
+                    'content': None,
+                    'tool_calls': [
+                        {
+                            'id': tc['id'],
+                            'type': 'function',
+                            'function': {
+                                'name': tc['name'],
+                                'arguments': tc['arguments'],
+                            }
+                        }
+                        for tc in tool_calls.values()
+                    ],
+                })
 
-                    print(f'Calling tool {name} with args {tool.function.arguments}')
+                for tool in tool_calls.values():
+                    name = tool['name']
+                    args = tool['arguments']
 
-                    result = await session.call_tool(name, arguments=args)
+                    print(f'\n\033[32m🛠️ Calling tool {name} with args {args}\033[0m', end='', flush=True)
+
+                    result = await session.call_tool(name, arguments=json.loads(args))
                     messages.append({
                         "role": "tool_response",
                         "content": result.content[0].text,
-                        "tool_call_id": tool.id,
+                        "tool_call_id": tool['id']
                     })
 
 if __name__ == '__main__':
